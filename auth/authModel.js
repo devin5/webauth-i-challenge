0c1dd85f9a640f5039e3c09899f47a2d2a87f313
Devin Dias <devindias.tech@gmail.com>
2019-11-12 20:38:14 -0600

    stuck on not receiving cookie

--- auth/authModel.js
+++ auth/authModel.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const db = require("../data/dbConfig");
 
-function add(user) {
-  return db("users").insert(user, "id");
-}
+const add = user => {
+  return db("users").insert(user);
+};
 
 function findBy(filter) {
   return db("users").where(filter);
 }
 
 function find() {
-    return db('users').select('id', 'username');
-  }
+  return db("users").select("id", "username");
+}
 
 module.exports = {
   add,
--- server.js
+++ server.js
@@ -1,17 +1,37 @@
-const express = require('express');
-const configureMiddleware = require('./middleware/configure-middleware');
+const express = require("express");
+const configureMiddleware = require("./middleware/configure-middleware");
+const session = require("express-session");
+const KnexSessionStorage = require("connect-session-knex")(session);
 
-const authRouter = require('./auth/authRouter.js');
+// const knexConnection = require("./data/dbConfig");
+
+const authRouter = require("./auth/authRouter.js");
 
 const server = express();
 
+const sessionConfiguration = {
+  name: "booger", // default name is sid
+  secret: process.env.COOKIE_SECRET || "is it secret? is it safe?",
+  cookie: {
+    maxAge: 1000 * 60 * 60, // valid for 1 hour (in milliseconds)
+    secure: process.env.NODE_ENV === "development" ? false : true, // do we send cookie over https only?
+    httpOnly: true // prevent client javascript code from accessing the cookie
+  },
+  resave: false, // save sessions even when they have not changed
+  saveUninitialized: true, // read about it on the docs to respect GDPR
+  store: new KnexSessionStorage({
+    knex: require("./data/dbConfig"),
+    clearInterval: 1000 * 60 * 10, // delete expired sessions every 10 minutes
+    tablename: "knexsessions",
+    sidfieldname: "sessionid",
+    createtable: true
+  })
+};
+
+// 3: use the session middleware globally
 configureMiddleware(server);
-
 server.use(express.json());
-
-server.use('/api/auth', authRouter);
-
-
-  
+server.use(session(sessionConfiguration));
+server.use("/api/auth", authRouter);
 
 module.exports = server;
